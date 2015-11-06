f6d8fde1e775181f365cdb6b7dd10b82baff12a6
Cynthia Subakat <cynn.d.s@gmail.com>
2015-11-06 11:13:42 +1100

    First commit

---
+++ RegexMatcher/Models/RegexModels.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace RegexMatcher.Models
+{
+    public class RegexModels
+    {
+        [DisplayName("String")]
+        public string stringToMatch { get; set; }
+
+        public string inputRegex { get; set; }        
+    }
+}
